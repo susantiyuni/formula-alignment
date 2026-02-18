a249fc7d52d4e05c3d864ab330becaa6f2ab4443
susantiyuni <36025078+susantiyuni@users.noreply.github.com>
2026-02-18 10:36:10 +0100

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -2,13 +2,16 @@
 
 set -e  # stop on error
 
-echo "Running dense_noalign baseline..."
+echo "Running dense_noalign..."
 python train.py --baseline_type dense_noalign
 
-echo "Running dual_encoder baseline..."
+echo "Running dual_encoder..."
 python train.py --baseline_type dual_encoder
 
-echo "Running simcse baseline..."
+echo "Running simcse..."
 python train.py --baseline_type simcse
+
+echo "Running GNN-CL..."
+python train_gnn_cl.py
 
 echo "All experiments completed."
