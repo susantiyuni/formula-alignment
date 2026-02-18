280dbec0b118fad11260087ed99bd22bb56c3a2a
susantiyuni <36025078+susantiyuni@users.noreply.github.com>
2026-02-18 10:41:26 +0100

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -3,10 +3,12 @@
 set -e  # stop on error
 
 echo "Running dense_noalign..."
-python train.py --baseline_type dense_noalign
+python train.py --baseline_type dense_noalign --encoder_type mathbert
+python train.py --baseline_type dense_noalign --encoder_type sbert
 
 echo "Running dual_encoder..."
-python train.py --baseline_type dual_encoder
+python train.py --baseline_type dual_encoder --encoder_type mathbert
+python train.py --baseline_type dual_encoder --encoder_type sbert
 
 echo "Running simcse..."
 python train.py --baseline_type simcse
