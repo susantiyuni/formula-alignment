e2202c9f3fe4779d01d239b54b7efbde41dba95f
susantiyuni <36025078+susantiyuni@users.noreply.github.com>
2026-02-18 10:50:54 +0100

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -5,16 +5,17 @@ set -e  # stop on error
 echo "Running BM25..."
 python bm25.py
 
-echo "Running dense_noalign..."
+echo "Running dense_noalign no CL..."
 python train.py --baseline_type dense_noalign --encoder_type mathbert
 python train.py --baseline_type dense_noalign --encoder_type sbert
 
-echo "Running dual_encoder..."
+echo "Running dual_encoder no CL..."
 python train.py --baseline_type dual_encoder --encoder_type mathbert
 python train.py --baseline_type dual_encoder --encoder_type sbert
 
-echo "Running simcse..."
-python train.py --baseline_type simcse
+echo "Running dual_encoder CL..."
+python dual_encoder.py --model_name all-mpnet-base-v2
+python dual_encoder.py --model_name tbs17/MathBERT
 
 echo "Running GNN-CL..."
 python train_gnn_cl.py
